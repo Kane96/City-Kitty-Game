0924c2a8b9f80860cf9ce81918b8c4bcafe55b73
KaneCr96 <k.crichton96@gmail.com>
2018-12-30 13:58:10 +0000

    Added basic project files

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rigidBody;
+
+    public float forceRight;
+    public float veloLimitRight;
+
+    public float jumpingForce;
+
+    void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+	}
+
+    void FixedUpdate()
+    {
+        if (!(rigidBody.velocity.x > veloLimitRight))
+        {
+            rigidBody.AddForce(Vector3.right * forceRight, ForceMode2D.Force);
+        }
+
+        print(rigidBody.velocity);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            print("Jumping!");
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpingForce);
+        }
+    }
+}
