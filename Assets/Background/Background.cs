292c6900dba605728ff5ec40bf4e982be233db11
KaneCr96 <k.crichton96@gmail.com>
2019-01-20 17:10:15 +0000

    Added movement to the background buildings.

---
+++ Assets/Background/Background.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+
+    public float veloRight;
+
+    private Rigidbody2D rigidBody;
+    private Vector2 moveVelo;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        moveVelo = new Vector2(veloRight, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody.velocity = moveVelo;
+    }
+}
