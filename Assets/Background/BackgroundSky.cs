375b24b6998fd1a4abc34f340ad7380835aa04cb
KaneCr96 <k.crichton96@gmail.com>
2019-01-20 17:23:59 +0000

    Background sky can now follow the player.

---
+++ Assets/Background/BackgroundSky.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundSky : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+	void Start ()
+    {
+        offset = transform.position;
+	}
+	void Update ()
+    {
+        Vector3 newPos = new Vector3(offset.x + player.transform.position.x, offset.y, 0);
+        transform.position = newPos;
+	}
+}
