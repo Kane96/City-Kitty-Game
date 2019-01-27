798c39117a6b7b1cb771ad8d3fd93447d95ccc7c
KaneCr96 <k.crichton96@gmail.com>
2019-01-27 13:46:11 +0000

    Implemented difficulty increase as time progresses.

--- Assets/Platforms/SegmentSpawner.cs
+++ Assets/Platforms/SegmentSpawner.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class SegmentSpawner : MonoBehaviour
 {
-    public GameObject[] segments;
+    public GameObject[] easySegments;
+    public GameObject[] normalSegments;
+    public GameObject[] hardSegments;
+
     private GameObject currentSegment;
+
+    // Difficulty is the gradually increasing index of the segments array.
+    private int difficulty = 0;
+    [Tooltip("In seconds")]
+    public float difficultyIncreaseFreq;
 
     private float totalWidth;
     private float spawnAt;
-    private float beginSpawningAt;
+    public float beginSpawningAt;
 
     private int random;
 
@@ -17,6 +25,8 @@ public class SegmentSpawner : MonoBehaviour
 
 	void Start ()
     {
+        // Unity crashes when initialising arrays of gameobject arrays...
+
         player = GameObject.FindGameObjectWithTag("Player");
         getRandomSeg();
         spawnSegment();
@@ -24,12 +34,18 @@ public class SegmentSpawner : MonoBehaviour
 
 	void Update ()
     {
+        int numberOfDifficulties = 3;
         Vector3 playerPos = player.GetComponent<Transform>().position;
         
         // If it's time to spawn a new level segment ahead of the player
         if (playerPos.x > spawnAt)
         {
             spawnSegment();
+        }
+        
+        if (Time.time >= difficultyIncreaseFreq && !(difficulty >= numberOfDifficulties))
+        {
+            increaseDifficulty();
         }
 	}
 
@@ -49,12 +65,43 @@ public class SegmentSpawner : MonoBehaviour
     {
         // Don't spawn the same segment twice in a row
         int randomTemp;
+        GameObject[] temp = getRandomSegArray();
         do
         {
-            randomTemp = Random.Range(0, segments.Length);
+            randomTemp = Random.Range(0, temp.Length);
         } while (randomTemp == random);
         random = randomTemp;
-        
-        currentSegment = segments[random];
+
+        currentSegment = temp[random];
     }
+
+    
+    
+    public GameObject[] getRandomSegArray()
+    {
+        
+        int randomTemp = Random.Range(0, difficulty);
+        switch (randomTemp)
+        {
+            case 0:
+                return easySegments;
+            case 1:
+                return normalSegments;
+            case 2:
+                return hardSegments;
+            default:
+                print("Error finding a random segment!");
+                return easySegments;
+        }
+        
+        return easySegments;
+    }
+    
+    
+    public void increaseDifficulty()
+    {
+        difficultyIncreaseFreq += Time.time;
+        difficulty++;
+    }
+    
 }
