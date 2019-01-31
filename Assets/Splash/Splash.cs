7adbeb86790320166482e15b1f9c747d31ac827b
KaneCr96 <k.crichton96@gmail.com>
2019-01-31 15:22:46 +0000

    Added splash screen.

---
+++ Assets/Splash/Splash.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Splash : MonoBehaviour
+{
+    public void loadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
